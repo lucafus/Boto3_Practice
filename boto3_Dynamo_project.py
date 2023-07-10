052458ec02d0c4f0976e6b939020e47ed72023d7
luca <luca.fusaro1996@gmail.com>
2023-07-10 17:56:08 -0300

    Python Project

---
+++ boto3_Dynamo_project.py
@@ -0,0 +1,39 @@
+pip install boto3
+
+import boto3
+
+dynamodb = boto3.resource('dynamodb', region_name= 'us-east-2')
+
+table = dynamodb.create_table( 
+    
+    TableName= 'Video_Games',
+    
+    AttributeDefinitions=[ 
+            
+            {
+            
+            'AttributeName': 'title', # Specify the name of the title attribute
+            'AttributeType' : 'S'     # Specify that the title attribute is a string
+            
+            }
+         ],
+   
+    KeySchema=[ 
+            
+            {   
+            
+            'AttributeName': 'title', # Specify that the title attribute will be used as the partition key
+            'KeyType' : 'HASH'        # Specify that the title attribute will be used as the hash key
+                        
+            }
+         ],
+                      
+    ProvisionedThroughput={
+        'ReadCapacityUnits': 1,
+        'WriteCapacityUnits' : 1
+          
+            }        
+)
+
+print(table)
+
