1fbb26d178cde9ead89099b1354e645116ad6d2a
lucafus <luca.fusaro1996@gmail.com>
2023-07-11 19:39:09 -0300

    Modification of lambda_handler

--- boto3_Dynamo_project_2.py
+++ boto3_Dynamo_project_2.py
@@ -132,17 +132,3 @@ table.put_item(
 )
 
 print("Put Items succeded")
-
-def lambda_handler(event, context):
-    table = dynamodb.Table('Gem_Games')
-
-    response = table.delete_item(
-        Key={
-            'title': "God of War 1"
-        }
-    )
-    
-    status_code = response['ResponseMetadata']['HTTPStatusCode']
-    print(status_code)
-
-    
