af3526d3d8b923d2acd2fd6e99de9a96dca464ad
lucafus <luca.fusaro1996@gmail.com>
2023-07-12 22:08:40 -0300

    comment changes

--- boto3_Dynamo_project_3.py
+++ boto3_Dynamo_project_3.py
@@ -1,35 +1,24 @@
 import boto3
 
-def lambda_handler(event, context):
-    # Create DynamoDB client
-    dynamodb = boto3.client('dynamodb')
-    table_name = 'Gem_Games'  
+dynamodb = boto3.client('dynamodb')
+table_name = 'Gem_Games'  
 
-    # Query an item from the table
-    item_to_query = 'ItemToQuery' 
-    query_response = dynamodb.get_item(
-        TableName='Gem_Games',
-        Key={
-            'PrimaryKey': {'S': item_to_query}
-        }
-    )
-    
-    # Retrieve the queried item
-    queried_item = query_response.get('Item')
-    if queried_item:
-        print('Queried item:', queried_item)
-    else:
-        print('Item not found')
+# Retrieve the queried item
+queried_item = query_response.get('God of War 1')
+if queried_item:
+    print('Queried item:', queried_item)
+else:
+    print('Item not found')
 
-    # Delete an item from the table
-    item_to_delete = 'ItemToDelete'  # Replace with the item you want to delete
-    delete_response = dynamodb.delete_item(
-        TableName=table_name,
-        Key={
-            'PrimaryKey': {'title': "God of War 1"}
+# Delete an item from the table
+item_to_delete = 'God of War 1'  
+delete_response = dynamodb.delete_item(
+    TableName=table_name,
+    Key={
+        'PrimaryKey': {'S': "God of War 1"}
         }
     )
 
-    print('Deletion response:', delete_response)
+print('Deletion response:', delete_response)
     
     
